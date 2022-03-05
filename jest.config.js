f1b9e8592b33e31457b01aa85af473431e7a7e3b
arx-8 <swans.randomness@gmail.com>
2022-03-05 23:49:24 +0900

    chore: Add jest

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+// @ts-check
+
+/**
+ * @type {import("@jest/types").Config.InitialOptions}
+ */
+const config = {
+  transform: {
+    "^.+\\.(t|j)sx?$": "@swc/jest",
+  },
+}
+
+module.exports = config
