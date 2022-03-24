3c4a60cdb500727ad4d8e971b0aefe2aeed884da
arx-8 <swans.randomness@gmail.com>
2022-03-24 12:19:49 +0900

    feat: Add example of API call

--- src/presentation/appHomeOpenedListener.ts
+++ src/presentation/appHomeOpenedListener.ts
@@ -1,12 +1,42 @@
 import { Middleware, SlackEventMiddlewareArgs } from "@slack/bolt"
+import axios from "axios"
+
+export type JapanZipCodeAPIResponse =
+  | {
+      message: null
+      results: {
+        address1: string
+        address2: string
+        address3: string
+        kana1: string
+        kana2: string
+        kana3: string
+        prefcode: string
+        zipcode: string
+      }[]
+      status: number
+    }
+  | {
+      message: string
+      results: null
+      status: number
+    }
 
 /**
  * Required permissions: Event Subscriptions > Subscribe to bot events > app_home_opened
  */
 export const appHomeOpenedListener: Middleware<
   SlackEventMiddlewareArgs<"app_home_opened">
-> = async ({ client, event, logger }) => {
+> = async ({ body, client, event, logger }) => {
   try {
+    // example of external api call
+    const resp = await axios.get<JapanZipCodeAPIResponse>(
+      "https://zipcloud.ibsnet.co.jp/api/search?zipcode=1000001"
+    )
+    const address = `${resp.data.results?.[0]?.address1 ?? ""} ${
+      resp.data.results?.[0]?.address2 ?? ""
+    } ${resp.data.results?.[0]?.address3 ?? ""}`
+
     const result = await client.views.publish({
       user_id: event.user,
       view: {
@@ -20,7 +50,14 @@ export const appHomeOpenedListener: Middleware<
           },
           {
             text: {
-              text: "Learn how home tabs can be more useful and interactive <https://api.slack.com/surfaces/tabs/using|*in the documentation*>.",
+              text: `Your team_id is ${body.team_id}`,
+              type: "mrkdwn",
+            },
+            type: "section",
+          },
+          {
+            text: {
+              text: `Your address is ${address}`,
               type: "mrkdwn",
             },
             type: "section",
